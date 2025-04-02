831f9348a1eb51c8edb3dc072ece0a47519fdd5b
han <icyfarhan@gmail.com>
2025-04-02 21:00:12 +0700

    Delete unused partials, updated the booking create function

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,5 +3,11 @@ class BookingsController < ApplicationController
   end
 
   def create
+    if authenticated?
+      user_email = Current.user.email_address
+    end
+
+    user = User.find_by(email_address: user_email)
+    user.flights.push(Flight.find(params[:flight_id]))
   end
 end
